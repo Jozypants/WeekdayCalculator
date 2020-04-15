dfadf97bee01f3838be3997e493c8b9cb9edef4f
Jozy Kinnaman <jozypants@gmail.com>
2020-04-15 10:18:10 -0700

    test failed. re-test spec removing 0

--- __tests__/weekdaycalculator.test.js
+++ __tests__/weekdaycalculator.test.js
@@ -3,9 +3,9 @@ import { RuleTester } from 'eslint';
 
 describe('Date', () => {
   test('should correctly display a date with month, day, year', () => {
-    let date = new Date(23, 01, 1986);
+    let date = new Date(23, 1, 1986);
     expect(date.day).toEqual(23);
-    expect(date.month).toEqual(01);
+    expect(date.month).toEqual(1);
     expect(date.year).toEqual(1986);
   });
 });
