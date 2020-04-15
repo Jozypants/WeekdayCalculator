fbbf73ccca06ea354eb4b1dbe29774768b4046c3
Jozy Kinnaman <jozypants@gmail.com>
2020-04-15 10:16:26 -0700

    test spec correctly display date constructor.

--- __tests__/weekdaycalculator.test.js
+++ __tests__/weekdaycalculator.test.js
@@ -3,9 +3,9 @@ import { RuleTester } from 'eslint';
 
 describe('Date', () => {
   test('should correctly display a date with month, day, year', () => {
-    let date = new Date(01, 23, 1986);
-    expect(date.day).toEqual(01);
-    expect(date.month).toEqual(25);
+    let date = new Date(23, 01, 1986);
+    expect(date.day).toEqual(23);
+    expect(date.month).toEqual(01);
     expect(date.year).toEqual(1986);
   });
 });
